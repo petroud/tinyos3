36eb6eee9f43495e24878e320068cc176126deef
petroud <dpetrou1@outlook.com>
2020-12-15 16:48:20 +0200

    Final Commit

--- kernel_pipe.c
+++ kernel_pipe.c
@@ -45,14 +45,15 @@ pipe_cb* pipe_init(){
 	return pipe;	
 }
 
-
+//Allocates the pointers in each side of the pipe
+//Then connects the stream functions to the operations
 int sys_Pipe(pipe_t* pipe)
 {
 
 	FCB *fcbs[2];
 	Fid_t fids[2];
 
-
+	//reserves an 2 FCBs to connect them to the pipe read and write end
 	int retval = FCB_reserve(2,fids,fcbs);
 
     if(retval == 0){
@@ -80,16 +81,18 @@ int sys_Pipe(pipe_t* pipe)
 }
 
 
-
+//Reads from a pipe
 int pipe_read(void* this, char* buffer, unsigned int size){
 	
-
+	//finds the pipe from casting *this object
 	pipe_cb* curPipe = (pipe_cb*)this;
 
+	//important error check, otherwise it cannot read the pipe
 	if(curPipe==NULL || size<=0 || curPipe->reader == NULL){
 		return -1;
 	}
 
+	//waits for the writers to write to the pipe, then reads it
 	while(curPipe->writer!=NULL && curPipe->capacity == PIPE_BUFFER_SIZE-1){
 		kernel_broadcast(&curPipe->has_space);
 		kernel_wait(&curPipe->has_data,SCHED_PIPE);
@@ -98,10 +101,13 @@ int pipe_read(void* this, char* buffer, unsigned int size){
 	int i = 0;
 	for(i=0; i<size; i++){
 
-	 	if(curPipe->writer==NULL && curPipe->capacity == PIPE_BUFFER_SIZE-1){
+		//in case the writer is closed, and all bytes have been read the readers returns
+	 	
+		 if(curPipe->writer==NULL && curPipe->capacity == PIPE_BUFFER_SIZE-1){
 			return i;
 		}
 
+		//Move the pointer to the buffer to read
 		buffer[i] = curPipe->BUFFER[curPipe->r_position];
 
 		curPipe->r_position = (curPipe->r_position + 1);
@@ -113,21 +119,23 @@ int pipe_read(void* this, char* buffer, unsigned int size){
 		curPipe->capacity++;
 	}
 
+	//the reading is finished and it informs the writers to write
 	kernel_broadcast(&curPipe->has_space);
 	return i;
 }
 
 
-
+//Writes to the pipe
 int pipe_write(void* this, const char* buffer, unsigned int size){
 
 	pipe_cb* curPipe = (pipe_cb*)this;
 	
-
+	//important error check, else it cannot write to the pipe
 	if(curPipe==NULL || size<=0 || curPipe->writer == NULL || curPipe->reader == NULL){
 		return -1;
 	}
 
+	//tells the reader that it has written, waits for the reader to read and empty the buffer
 	while(curPipe->reader!=NULL && curPipe->capacity == 0){
 			kernel_broadcast(&curPipe->has_data);
 			kernel_wait(&curPipe->has_space,SCHED_PIPE);
@@ -136,6 +144,7 @@ int pipe_write(void* this, const char* buffer, unsigned int size){
 	int i = 0;
 	for(i=0; i<size; i++){
 		
+		//if reader does not exsist, the writer has no job left
 		if(curPipe->writer == NULL || curPipe->reader == NULL){
 			return -1;
 		}	
@@ -149,6 +158,7 @@ int pipe_write(void* this, const char* buffer, unsigned int size){
 			return -1;
 		}
 
+		//Move the pointer to the buffer to write
 		curPipe->BUFFER[curPipe->w_position] = buffer[i];
 
 		curPipe->w_position = (curPipe->w_position+1);
@@ -157,6 +167,7 @@ int pipe_write(void* this, const char* buffer, unsigned int size){
 			curPipe->w_position = 0;
 		}
 
+		//update capacity variable
 		curPipe->capacity--;
 	}
 
@@ -166,24 +177,25 @@ int pipe_write(void* this, const char* buffer, unsigned int size){
 
 
 
-
+//Closes the reader
 int pipe_close_reader(void* this){
-
+	
 	pipe_cb* curPipe = (pipe_cb*)this;
 
 	if(curPipe==NULL || curPipe->reader == NULL){
 		return -1;
 	}
-	
-	curPipe->reader=NULL;
 
+	//closes the reader, making him point to a null location
+	curPipe->reader=NULL;
+	//frees the space the pipe was in, if the writer does not exist too
 	if(curPipe->writer == NULL){
 		free(curPipe);
 	}
 	return 0;
 }
 
-
+//Closes the writer
 int pipe_close_writer(void* this){
 
 	pipe_cb* curPipe = (pipe_cb*)this;
@@ -191,11 +203,15 @@ int pipe_close_writer(void* this){
 		return -1;
 	}
 
+
+	//closes the writer, making him point to a null location
 	curPipe->writer=NULL;
+	//call the readers to read it possible data left in the buffer
 	if(curPipe->reader!=NULL){
 		kernel_broadcast(&curPipe->has_data);
 	}
-
+	
+	//Then frees the space the pipe was in
 	if(curPipe->reader==NULL){
 		free(curPipe);
 	}
